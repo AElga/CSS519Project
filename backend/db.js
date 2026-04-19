620b47c9499847f68c7c2f3b00dac644458d4081
Ali Elgazzar <121975641+AElga@users.noreply.github.com>
2026-04-19 16:08:02 -0700

    Adjusted

--- backend/db.js
+++
@@ -1,7 +0,0 @@
-const path = require("path");
-const sqlite3 = require("sqlite3").verbose();
-
-const dbPath = process.env.DB_PATH || path.join(__dirname, "elghealth.db");
-const db = new sqlite3.Database(dbPath);
-
-module.exports = db;
